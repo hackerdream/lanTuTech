6825008908462131c92caf34c7a8263b894059d9
GraySpider <76154662@qq.com>
2017-11-03 16:34:27 +0800

    finish the nav

---
+++ js/index.js
@@ -0,0 +1,8 @@
+(function () {
+    $(function () {
+        $(".min-nav-menu").click(function () {
+            // console.log("111");
+            $(".nav-hid-menu").toggle();
+        })
+    })
+})()
